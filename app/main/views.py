f108acc29cca866fd79a6319c4271ef6ed447fbd
Jackie Macharia <jacklinewmacharia@gmail.com>
2016-11-23 01:19:30 +0300

    Add error handlers

--- app/main/views.py
+++ app/main/views.py
@@ -16,3 +16,16 @@ def index():
     Landing page when app url is keyed in browser
     """
     return render_template('index.html')
+
+
+@main_blueprint.errorhandler(403)
+def not_allowed(e):
+    return render_template('403.html'), 403
+
+@main_blueprint.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@main_blueprint.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
